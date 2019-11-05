d84350c2453a3d289146971e4e6002b63afab773
Andrea Evans <andreaevans@pursuit.org>
2019-11-05 15:35:23 -0500

    created labels and buttons, connected to viewController

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userAnswer: UITextField!
+    
+    @IBOutlet weak var scrambledWord: UILabel!
+    
+     let rando = Word.getRandomWord()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+// functions and methods
+    
+    func scrambledWordPresented() {
+        scrambledWord.text = rando
+    }
+    @IBAction func nextWordButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
